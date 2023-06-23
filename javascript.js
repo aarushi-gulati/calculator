5125af899d689a3505c7f85fb2e5d624511dd443
aarushi-gulati <aarushigulati07@gmail.com>
2023-06-23 22:28:13 +0530

    Add calculations

--- javascript.js
+++ javascript.js
@@ -16,11 +16,11 @@ function divide(num1, num2){
 
 function operate(num1, num2, operator){
     if (operator === '+'){
-        return add(num1 + num2)
+        return add(num1, num2)
     }
 
     else if (operator === '-'){
-        return subtract(num1 - num2)
+        return subtract(num1, num2)
     }
 
     else if (operator === '*'){
@@ -32,3 +32,50 @@ function operate(num1, num2, operator){
     }
 }
 
+const expression = [0];
+
+let num = "";
+
+let op = "";
+
+let num2 = 0;
+
+let res = 0;
+
+buttons = document.querySelectorAll("button");
+
+const result = document.querySelector("p");
+
+const operators = ['+', '-', '*', '/'];
+
+result.textContent = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        keyPressed = button.id;
+
+        if (operators.includes(keyPressed)){
+            if (op === ""){
+                num2 = num;
+                op = keyPressed;
+                num = "";
+            }
+            else{
+                res = operate(parseInt(num2), parseInt(num), op);
+                num2 = res;
+                op = keyPressed;
+                num = "";
+                result.textContent = res;
+            }
+        }
+        else if (keyPressed == '='){
+            res = operate(parseInt(num2), parseInt(num), op);
+            num2 = res;
+            result.textContent = res;
+        }
+        else{
+            num += keyPressed;
+            result.textContent = num;
+        }
+    });
+});
